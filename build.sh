7710d96d04229a35c881cd5b7def5d5bbece2318
Benjamin Borbe <bborbe@nova.fritz.box>
2017-01-27 19:24:05 +0100

    update build

--- build.sh
+++ build.sh
@@ -5,14 +5,14 @@ set -e
 make checkout
 
 tag=$(cd sources;git tag -l --points-at HEAD)
-if ! test -z "$tag"; then
+if [ ! -z "$tag" ]; then
 	VERSION=${tag} make build
 	VERSION=${tag} make upload
 	VERSION=${tag} make clean
 fi
 
 branch=$(cd sources;git rev-parse --abbrev-ref HEAD)
-if ! "$branch" = "HEAD"; then
+if [ ! "$branch" = "HEAD" ]; then
 	VERSION=${branch} make build
 	VERSION=${branch} make upload
 	VERSION=${branch} make clean
