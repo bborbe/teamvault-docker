705b87505ee9c5f877349c55263318ae9f1a573e
Benjamin Borbe <bborbe@nova.local>
2017-02-06 13:06:41 +0100

    allow clean to fail

--- build.sh
+++ build.sh
@@ -8,12 +8,12 @@ tag=$(cd sources;git tag -l --points-at HEAD)
 if [ ! -z "$tag" ]; then
 	VERSION=${tag} make build
 	VERSION=${tag} make upload
-	VERSION=${tag} make clean
+	VERSION=${tag} make clean || true
 fi
 
 branch=$(cd sources;git rev-parse --abbrev-ref HEAD)
 if [ ! "$branch" = "HEAD" ]; then
 	VERSION=${branch} make build
 	VERSION=${branch} make upload
-	VERSION=${branch} make clean
+	VERSION=${branch} make clean || true
 fi
